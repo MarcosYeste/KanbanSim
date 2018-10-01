31ccab1ef3563761526ddce7cc69e69309a718da
MARCOS YESTE MOLINA <6001123@insags.com>
2018-10-01 13:11:33 +0200

    controler&Service

--- src/main/java/com/kanban/app/HomeController.java
+++ src/main/java/com/kanban/app/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,7 +28,7 @@ public class HomeController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		DateFormat dateFormat = DateFormat.getDateTimeInstance( DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
@@ -35,13 +36,23 @@ public class HomeController {
 		
 		return "home";
 	}
+	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String home(Model model) {		
+	public String addKanban(Model model) {		
 		
-		// AQUI DEBO PASAR VALORES VACIOS PARA EL FORMULARIO
 		String kanbanMSG = "This is a Kanban form";
-		
+		model.addAttribute("fases", new Phase());
 		model.addAttribute("kanbanMSG", kanbanMSG );
+		
+		return "kanbanForm";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String Kanban(Model model, @RequestBody Phase fases) {		
+		
+		model.addAttribute("fases", saveFases(fases));
+		
+		
 		
 		return "kanbanForm";
 	}
---
+++ src/main/java/com/kanban/app/services/KanbanService.java
@@ -0,0 +1,14 @@
+package com.kanban.app.services;
+
+import org.springframework.stereotype.Service;
+
+import com.kanban.app.Phase;
+
+@Service
+public class KanbanService {
+	
+	public saveFases (Phase fases) {
+		
+	}
+
+}
