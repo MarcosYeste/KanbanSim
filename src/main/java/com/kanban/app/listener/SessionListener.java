4a2108769f727d382e45fbe4ca464fcada5a0b8f
MARCOS YESTE MOLINA <6001123@insags.com>
2019-01-24 11:23:57 +0100

    x

---
+++ src/main/java/com/kanban/app/listener/SessionListener.java
@@ -0,0 +1,36 @@
+package com.kanban.app.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class SessionListener
+ *
+ */
+public class SessionListener implements HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+	 private static int totalActiveSessions = 0;
+    public SessionListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent session)  { 
+    	totalActiveSessions += 1;
+    	session.getSession().setAttribute("visitas", totalActiveSessions);
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent session)  {
+    	totalActiveSessions -= 1;
+    	session.getSession().setAttribute("visitas", totalActiveSessions);
+    }
+	
+}
